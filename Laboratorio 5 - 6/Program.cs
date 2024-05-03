a9d2f5ba9c6b9364a32329c8ebc8488ba66d3642
jhunnior06 <157752136+jhunnior06@users.noreply.github.com>
2024-05-02 21:14:23 -0500

    c

---
+++ Laboratorio 5 - 6/Program.cs
@@ -0,0 +1,39 @@
+﻿//4.Desarrollo y Actividades Ejercicio: 
+
+//Recursividad:
+//1) Ejercicio 1: Escribe una función recursiva que imprima los números pares del 1 al 100. 
+
+Console.WriteLine("Ingrese un número para elegir una opción:");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("===================== Ejercicio 1 =====================");
+    for (int i = 2; i <= 100; i += 2)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+//2) Ejercicio 2: Escribe una función recursiva que imprima la suma de los números del 1 al n. 
+
+else if (a == 2)
+{ 
+    Console.WriteLine("===================== Ejercicio 2 =====================");
+Console.WriteLine("Ingrese un número :");
+int n = int.Parse(Console.ReadLine());
+int suma = SumaNumeros(n);
+Console.WriteLine("La suma de los números del 1 al " + n + " es: " + suma);
+    }
+
+    static int SumaNumeros(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n + SumaNumeros(n - 1);
+    }
+}
